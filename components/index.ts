25c9e56d0a58272aa81b46d48ca55832bc204d1c
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-11-06 16:42:53 +0300

    components exporter

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+export { default as VideoPlayer } from './VideoPlayer';
+export { default as VideoControls } from './VideoControls';
+export { default as VideoDetails } from './VideoDetails';
+export { default as JumpLoader } from './JumpLoader';
