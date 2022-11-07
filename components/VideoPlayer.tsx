24eecca73f9b3ac3b1cf3d4a8af309803afe4c14
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-11-07 12:22:14 +0300

    bug fix: when the url changes, video url updates

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,4 +1,6 @@
-import React, { FC } from 'react';
+// @ts-nocheck
+import React, { FC, useEffect, useRef } from 'react';
+import styles from '../styles/videos.module.css';
 
 type VideoPlayerProp = {
 	videoId: number;
@@ -6,10 +8,26 @@ type VideoPlayerProp = {
 };
 
 const VideoPlayer: FC<VideoPlayerProp> = ({ videoId, serverUrl }) => {
+	const videoRef = useRef();
+
+	useEffect(() => {
+		videoRef?.current?.load();
+	}, [videoId]);
+
 	return (
-		<video id="videoPlayer" controls muted autoPlay>
-			<source src={`${serverUrl}/${videoId}`} type="video/mp4" />
-		</video>
+		<div className={styles.playerContainer}>
+			<video
+				ref={videoRef}
+				id="videoPlayer"
+				// hacky solution to reload on url change
+				// key={`${serverUrl}/${videoId}`}
+				controls
+				muted
+				autoPlay
+			>
+				<source src={`${serverUrl}/${videoId}`} type="video/mp4" />
+			</video>
+		</div>
 	);
 };
 
